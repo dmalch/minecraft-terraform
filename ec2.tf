8ffd175834585fb24d305cccaf0c06af2e925e21
Dmitry Malchikov <dmalch@gmail.com>
2021-08-27 11:21:04 +0200

    Fix subnets, upgrade ubuntu

--- ec2.tf
+++ ec2.tf
@@ -2,14 +2,19 @@ module "ec2-instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 2.0"
 
-  name = "minecraft-public"
+  name = "minecraft-ec2"
 
-  key_name             = aws_key_pair.ec2_ssh[0].key_name
-  ami                  = data.aws_ami.ubuntu.image_id
-  instance_type        = "t2.micro"
+  ami = data.aws_ami.ubuntu.image_id
+
+  associate_public_ip_address = true
+
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [module.security-group.security_group_id]
+
+  key_name = aws_key_pair.ec2_ssh[0].key_name
+
   iam_instance_profile = aws_iam_instance_profile.minecraft.id
 
-  subnet_id                   = sort(data.aws_subnet_ids.default.ids)[0]
-  vpc_security_group_ids      = [module.security-group.security_group_id]
-  associate_public_ip_address = true
+  subnet_id = sort(module.vpc.public_subnets)[0]
 }
--- vpc.tf
+++ vpc.tf
@@ -4,15 +4,6 @@ module "vpc" {
   name = "minecraft-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  # Single NAT Gateway
-  enable_nat_gateway = true
-  single_nat_gateway = true
-}
-
-data "aws_subnet_ids" "default" {
-  vpc_id = module.vpc.vpc_id
+  azs             = ["${local.region}a"]
+  public_subnets  = ["10.0.101.0/24"]
 }
