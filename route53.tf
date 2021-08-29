ae596c299eb8ebbcb7339e0ce31acca1dbf16e91
Dmitry Malchikov <dmalch@gmail.com>
2021-08-29 20:13:57 +0200

    Route 53 config

---
+++ route53.tf
@@ -0,0 +1,19 @@
+resource "aws_route53_zone" "mcisar" {
+  name = "mcisar.com"
+}
+
+resource "aws_route53_record" "minecraft" {
+  zone_id = aws_route53_zone.mcisar.zone_id
+  name    = "mcisar.com"
+  type    = "A"
+  ttl     = "300"
+  records = module.ec2-instance.public_ip
+}
+
+resource "aws_route53_record" "minecraft-srv" {
+  zone_id = aws_route53_zone.mcisar.zone_id
+  name    = "mcisar.com"
+  type    = "SRV"
+  ttl     = "300"
+  records = ["0 5 25565 mcisar.com"]
+}
