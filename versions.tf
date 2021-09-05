fb463592e85dbad16ccb7d9e39583c0f1ca4585f
Dmitry Malchikov <dmalch@gmail.com>
2021-09-05 22:59:13 +0200

    S3 Backend

--- versions.tf
+++ versions.tf
@@ -5,6 +5,12 @@ terraform {
       version = "3.55.0"
     }
   }
+
+  backend "s3" {
+    bucket = "dmalch-terraform-state"
+    key    = "prod/terraform.tfstate"
+    region = "eu-central-1"
+  }
 }
 
 provider "aws" {
