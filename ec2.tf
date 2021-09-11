71d02c6936aee754fa2f239462cb129bacd1fec3
Dmitry Malchikov <dmalch@gmail.com>
2021-09-12 00:50:08 +0200

    Remove second server

--- ec2.tf
+++ ec2.tf
@@ -1,51 +1,14 @@
-module "ec2-instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 2.0"
-
-  name = "minecraft-ec2"
-
-  ami = data.aws_ami.ubuntu.image_id
-
+resource "aws_instance" "minecraft-ec2" {
+  ami                         = data.aws_ami.ubuntu.image_id
+  iam_instance_profile        = aws_iam_instance_profile.minecraft.id
+  instance_type               = "t2.medium"
+  subnet_id                   = sort(module.vpc.public_subnets)[0]
+  vpc_security_group_ids      = [module.security-group.security_group_id]
   associate_public_ip_address = true
-
-  instance_type = "t2.medium"
-
-  vpc_security_group_ids = [module.security-group.security_group_id]
-
-  key_name = aws_key_pair.ec2_ssh[0].key_name
-
-  iam_instance_profile = aws_iam_instance_profile.minecraft.id
-
-  subnet_id = sort(module.vpc.public_subnets)[0]
+  key_name                    = aws_key_pair.ec2_ssh[0].key_name
 
   user_data = templatefile("${path.module}/run_minecraft.sh", {
     minecraft_version = "latest"
     minecraft_bucket  = local.bucket
-  })
-}
-
-module "ec2-instance-2" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 2.0"
-
-  name = "minecraft-ec2-2"
-
-  ami = data.aws_ami.ubuntu.image_id
-
-  associate_public_ip_address = true
-
-  instance_type = "t2.medium"
-
-  vpc_security_group_ids = [module.security-group.security_group_id]
-
-  key_name = aws_key_pair.ec2_ssh[0].key_name
-
-  iam_instance_profile = aws_iam_instance_profile.minecraft.id
-
-  subnet_id = sort(module.vpc.public_subnets)[0]
-
-  user_data = templatefile("${path.module}/run_minecraft.sh", {
-    minecraft_version = "latest"
-    minecraft_bucket  = local.bucket2
   })
 }
--- route53.tf
+++ route53.tf
@@ -7,7 +7,7 @@ resource "aws_route53_record" "minecraft" {
   name    = "mcisar.com"
   type    = "A"
   ttl     = "300"
-  records = module.ec2-instance.public_ip
+  records = [aws_instance.minecraft-ec2.public_ip]
 }
 
 resource "aws_route53_record" "minecraft-srv" {
@@ -16,20 +16,4 @@ resource "aws_route53_record" "minecraft-srv" {
   type    = "SRV"
   ttl     = "300"
   records = ["0 5 25565 mcisar.com"]
-}
-
-resource "aws_route53_record" "fun-minecraft" {
-  zone_id = aws_route53_zone.mcisar.zone_id
-  name    = "fun.mcisar.com"
-  type    = "A"
-  ttl     = "300"
-  records = module.ec2-instance-2.public_ip
-}
-
-resource "aws_route53_record" "fun-minecraft-srv" {
-  zone_id = aws_route53_zone.mcisar.zone_id
-  name    = "fun.mcisar.com"
-  type    = "SRV"
-  ttl     = "300"
-  records = ["0 5 25565 fun.mcisar.com"]
 }
