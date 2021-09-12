88dde1c56a20cdc4adb7afe904e885b1adbaef5d
Dmitry Malchikov <dmalch@gmail.com>
2021-09-13 00:48:25 +0200

    Docker host added

--- ec2.tf
+++ ec2.tf
@@ -13,17 +13,17 @@ resource "aws_instance" "minecraft-ec2" {
   })
 }
 
-# resource "aws_instance" "docker-ec2" {
-#   ami                         = data.aws_ami.ubuntu.image_id
-#   iam_instance_profile        = aws_iam_instance_profile.minecraft.id
-#   instance_type               = "t2.micro"
-#   subnet_id                   = sort(module.vpc.public_subnets)[0]
-#   vpc_security_group_ids      = [module.security-group.security_group_id]
-#   associate_public_ip_address = true
-#   key_name                    = aws_key_pair.ec2_ssh[0].key_name
+data "template_file" "cloud_init_docker" {
+  template = file("cloud-init-docker.yaml")
+}
 
-#   user_data = templatefile("${path.module}/run_minecraft.sh", {
-#     minecraft_version = "latest"
-#     minecraft_bucket  = local.bucket
-#   })
-# }
+resource "aws_instance" "docker-ec2" {
+  ami                         = data.aws_ami.ubuntu.image_id
+  iam_instance_profile        = aws_iam_instance_profile.minecraft.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.minecraft_public.id
+  vpc_security_group_ids      = [aws_security_group.minecraft_ec2_ssh_and_minecraft.id]
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.ec2_ssh[0].key_name
+  user_data                   = data.template_file.cloud_init_docker.rendered
+}
