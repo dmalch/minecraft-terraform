33cb565bd019ccc9418cf474ca59d2e79ae63503
Dmitry Malchikov <dmalch@gmail.com>
2021-09-17 09:02:32 +0200

    Update docker host config

--- ec2.tf
+++ ec2.tf
@@ -22,7 +22,7 @@ data "template_file" "cloud_init_docker" {
 resource "aws_instance" "docker-ec2" {
   ami                         = data.aws_ami.ubuntu.image_id
   iam_instance_profile        = aws_iam_instance_profile.docker-instance-profile.id
-  instance_type               = "t2.micro"
+  instance_type               = "t2.medium"
   subnet_id                   = aws_subnet.minecraft_public.id
   vpc_security_group_ids      = [aws_security_group.minecraft_ec2_ssh_and_minecraft.id]
   associate_public_ip_address = true
